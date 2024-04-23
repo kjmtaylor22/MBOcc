43450ee89a4599549ae7ea7cdf962d8bc7743a9f
kjmtaylor22 <kjmtaylor.22@gmail.com>
2024-04-23 12:15:48 -0400

    modified:   R/format_data.R new file:   data.RD

--- R/format_data.R
+++ R/format_data.R
@@ -5,11 +5,28 @@
 #' @param
 #' @param
 #' @param
+#' @param zeroes Minimum number of samples from which a species can be absent.
 #' @export
 
-format <- function(comm, meta, tax, zeroes){
+format <- function(comm, meta, tax, zeroes, states){
 
   library(dplyr)
+
+  reframe <- function(x, states){
+    y <- dplyr::mutate(x, state=paste(contains(unique(states)), sep=""))
+    z <- reshape2::dcast(y, . ~ state)
+    return(z)
+  }
+
+  add.states <- function(x){
+    states <- names(trans.list[[1]])
+    missing.states <- which(!states%in%names(x))
+    add.states <- matrix(0,nrow(x),length(missing.states),dimnames=list(NULL,states[missing.states]))
+    y <- cbind(x, add.states)
+    return(y)
+  }
+
+
 
   if ("datatables"%in%class(tax)){
     taxonomy <- function(taxrow){
@@ -23,7 +40,7 @@ format <- function(comm, meta, tax, zeroes){
     }
     tax <- tax$x$data
     tax <- cbind(tag=tax[,1], tax[,-1], taxonomy=apply(tax, 1, taxonomy)) %>%
-      mutate(otu.name=paste0(taxonomy, " (", tag, ")"))
+      mutate(tag.name=paste0(taxonomy, " (", tag, ")"))
   }
 
   if (nrow(comm)!=nrow(meta)|ncol(comm)!=nrow(tax)){
@@ -45,8 +62,41 @@ format <- function(comm, meta, tax, zeroes){
   comm <- comm[row.names(meta),]
   comm <- comm[,row.names(tax)]
 
+  # remove species for which they are absent more than the threshold in param `zeroes`
+  pull <- apply(comm, 2, function(x){length(x[which(x==0)])})
+  comm <- comm[,-which(pull>zeroes)]
 
+  # reduce taxonomy to match reduced community table
+  tax <- tax[colnames(comm),]
 
+  # create a new data frame with the reduced information
+  taxa <- t(comm) %>% as.data.frame() %>%
+    tibble::rownames_to_column(var="tag") %>%
+    left_join(tax[,c("tag","taxonomy","tag.name")]) %>%
+    as.data.frame() %>% `row.names<-`(.$tag.name) %>%
+    .[,-c(1,(ncol(.)-2):ncol(.))] %>% t()
 
+  # make sure SampleID is specified in the metadata
+  meta <- data.frame(SampleID=row.names(meta), meta)
 
+  comm.list <- list()
+  for (i in colnames(taxa)){
+
+    tax.comm <- data.frame(SampleID=row.names(taxa), Taxon=taxa[,i]) %>%
+      left_join(meta) %>% droplevels(.)
+
+    if (!is.null(states)){
+      tax.comm <- reshape2::dcast(tax.comm, . ~ vars(states), value.var = "Taxon")
+    }
+
+    if (any(is.na(tax.comm))){tax.comm[is.na(tax.comm)] <- 0}
+
+    comm.list[[i]] <- tax.comm
+  }
+
+  if (!is.null(states)){
+    trans.list <- lapply(comm.list, reframe, states=states)
+    stand.list <- lapply(trans.list, add.states)
+  }
+  return(stand.list)
 }
